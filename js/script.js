23850273499a99b91fd4c3337957061a56cf1e68
yura.yushkevich <yura.yushkevich@gmail.com>
2021-12-20 10:06:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+
+// alert('hello');
+
+// const resault = confirm('Are here?');
+// console.log(resault);
+
+
+const answer = +prompt('Вам есть 18 лет?', 'Да');
+
+
+console.log(answer);
+
+console.log(8%2);
